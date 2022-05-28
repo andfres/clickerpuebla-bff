26065c45825020e64d0dd7d6dc6f0b0aeca82f1a
Andrés Fresno <andfres@gmail.com>
2022-05-28 21:16:02 +0200

    primer commit

---
+++ sistema-blog-spring-boot-api-rest/src/main/java/com/sistema/blog/SistemaBlogSpringBootApiRestApplication.java
@@ -0,0 +1,61 @@
+package com.sistema.blog;
+
+import com.sistema.blog.dto.ComentarioDTO;
+import com.sistema.blog.dto.PublicacionDTO;
+import com.sistema.blog.entidades.Publicacion;
+import com.sistema.blog.servicio.ComentarioServicio;
+import com.sistema.blog.servicio.PublicacionServicio;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+
+@SpringBootApplication
+public class SistemaBlogSpringBootApiRestApplication {
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaBlogSpringBootApiRestApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
+
+	@Bean
+	CommandLineRunner run(PublicacionServicio publicacionServicio, ComentarioServicio comentarioServicio) {
+		return args -> {
+
+			PublicacionDTO publicacion = new PublicacionDTO();
+			publicacion.setTitulo("titulooooooooo");
+			publicacion.setDescripcion("esto es una publicacion de prueba");
+			publicacion.setContenido("esto es el contenido de la pubblicacion");
+			publicacionServicio.crearPublicacion(publicacion);
+
+
+			ComentarioDTO aaaaa = new ComentarioDTO();
+			aaaaa.setNombre("EEEE");
+			aaaaa.setEmail("ee@gmail.com");
+			aaaaa.setCuerpo("EEEEsdfsdfdsfsdfsdfsdfsdfsdfdsfsdfsdfdsf");
+
+			comentarioServicio.crearComentario(1L, aaaaa);
+
+
+		};
+	}
+}
